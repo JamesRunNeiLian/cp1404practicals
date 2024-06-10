8ce037ef7e05138c23d1eef237f23b2af1b38c80
jamesrunneilian <jamesruneilian@my.jcu.edu.au>
2024-06-10 20:11:45 +0800

    importing random

---
+++ prac_03/randoms.py
@@ -0,0 +1,14 @@
+# I see 20 on line 1
+# The smallest number is 5
+#The largest number is 20
+
+# I see 9 in line 2
+# The smallest number we could have seen is 3 and the largest is 9
+# No line 2 would not produce 4 as it could produce onyl odd numbers because the range increments by 2 starting from 3, so only 3, 5, 7, and 9 are possible.
+#
+# I saw 3.903742105529359 on line 3
+# The smallest number is 2.5 and the largest is 5.5
+
+import random
+random_number = random.randint(1,100 )
+print(random_number)
