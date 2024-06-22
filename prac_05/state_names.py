bd117a0718ffb706c51729c29c259a234a5d7db9
jamesrunneilian <jamesruneilian@my.jcu.edu.au>
2024-06-22 12:15:32 +0800

    changing into EAFP Method

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,18 +4,22 @@ State names in a dictionary
 File needs reformatting
 """
 
+# Dictionary of state codes and their names
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales",
+                "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria",
+                "TAS": "Tasmania"}
 
-CODE_TO_NAME = {"QLD":"Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+# Print all state codes and names neatly lined up
+print("All state codes and names:")
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:<3} is {name}")
 
-state_code = input("Enter short state: ")
+# Input and lookup functionality using EAFP
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    try:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
-
-
-
+    state_code = input("Enter short state: ").upper()
