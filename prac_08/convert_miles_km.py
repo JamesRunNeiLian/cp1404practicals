3f4f723702e16c598e6eb2a8eb5edd6a2a7e7359
jamesrunneilian <jamesruneilian@my.jcu.edu.au>
2024-07-27 16:38:33 +0800

    Add handle_increment function

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -13,3 +13,16 @@ class MilesConverterApp(App):
         self.title = "Miles to Kilometres Converter"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_calculate(self, text):
+        """Handle calculation (could be button press or other call)."""
+        print("Calculating kilometers")
+        miles = self.convert_to_number(text)
+        self.update_result(miles)
+
+    def handle_increment(self, text, change):
+        """Handle up/down button press, update the text input with new value, call calculation function."""
+        print("Incrementing/Decrementing miles")
+        miles = self.convert_to_number(text) + change
+        self.root.ids.input_miles.text = str(miles)
+        # TextInput on_text event will trigger handle_calculate
