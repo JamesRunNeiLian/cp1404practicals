aa72fee2bcb2c8cd047833d1c5f69e3dff0ca087
jamesrunneilian <jamesruneilian@my.jcu.edu.au>
2024-07-27 17:11:29 +0800

    adding dynami label

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -3,6 +3,7 @@ from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 
+
 class DynamicLabelsApp(App):
     """Kivy App for dynamically creating labels from a list of names."""
 
@@ -13,4 +14,14 @@ class DynamicLabelsApp(App):
         # Load the kv file
         self.root = Builder.load_file('dynamic_labels.kv')
 
+        # Add labels dynamically
+        for name in names:
+            # Create a Label for each name
+            label = Label(text=name, size_hint_y=None, height=40)
+            # Add the Label to the BoxLayout with id 'main'
+            self.root.ids.main.add_widget(label)
+
         return self.root
+
+
+DynamicLabelsApp().run()
